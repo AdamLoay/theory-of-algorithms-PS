8ea7a5698d00c5f410a2a6b42161b86735004296
Adam Loay <adaamloay@proton.me>
2026-02-27 01:21:32 +0200

    [+]

---
+++ Print Even Indices/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int arr[], int i) {
+    if (i < 0) return;
+
+    if (i % 2 == 0) cout << arr[i] << " ";
+    print(arr, i - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    print(arr, n - 1);
+    return 0;
+}
