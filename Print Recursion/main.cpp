74d94ed287f1084d5c6df1b353c5779dfdfd14e9
Adam Loay <adaamloay@proton.me>
2026-02-26 17:06:54 +0200

    [+]

--- Print Recursion/main.cpp
+++ Print Recursion/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+
+void print(int n) {
+    if (n == 0) return;
+    std::cout << "I love Recursion\n";
+    print(n-1);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+    print(n);
     return 0;
 }
